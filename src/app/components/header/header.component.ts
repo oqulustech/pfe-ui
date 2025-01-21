36615dd2dbe3e8e045b46c4bf490045a169bd481
oqulustech <oqulustch@gmail.com>
2025-01-21 11:22:14 -0500

    today's changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { OmniNgModule } from '@cof/omni-ng';
+import { OmniFormsModule } from '@cof/omni-ng'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [OmniNgModule, OmniFormsModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
 }
