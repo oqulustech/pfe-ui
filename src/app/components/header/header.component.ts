dd3a897adeb5647490af9fec60c23c4e839de8da
oqulustech <oqulustch@gmail.com>
2025-01-23 14:36:05 -0500

    updated 5

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { OmniNgModule } from '@cof/omni-ng';
-import { OmniFormsModule } from '@cof/omni-ng'
+import { OmniFormsModule, OmniMenuButtonModule } from '@cof/omni-ng';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [OmniNgModule, OmniFormsModule ],
+  imports: [FormsModule, OmniNgModule, OmniFormsModule,OmniMenuButtonModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  value: string = '';
 }
