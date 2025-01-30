fb8adba0aa74a9f82f9089d06f73762480dd77d2
oqulustech <oqulustch@gmail.com>
2025-01-31 01:08:22 +0530

    asfsad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { JobMonitorComponent } from './features/job-monitor/job-monitor.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: JobMonitorComponent },
     { path: 'home', component: HomeComponent },
-    { path : "job-monitor", component: JobMonitorComponent }
+    { path : "job-monitor", component: JobMonitorComponent },
+    { path : "dashboard", component: DashboardComponent }
 ];
