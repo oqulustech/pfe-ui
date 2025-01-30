829b73c4a30b21e45f1fef8266390d3ee1bf2b32
oqulustech <oqulustch@gmail.com>
2025-01-30 12:26:40 -0500

    bye

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
+// import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+// import { OmniNgModule } from '@cof/omni-ng'
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [
+//     OmniNgModule,
+//     RouterModule
+//   ],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.scss'
+// })
+// export class AppComponent {
+// }
+
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { OmniNgModule } from '@cof/omni-ng'
 import { CommonModule } from '@angular/common';
-import { OmniNgModule } from '@cof/omni-ng';
 import { OmniAppModule } from '@cof/omni-ng';
 import { RouterOutlet } from '@angular/router';
-import { RouterModule, Routes } from '@angular/router';
 import { OmniContainerModule } from '@cof/omni-ng';
-
-
-
 // import components
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, OmniNgModule, OmniAppModule, OmniContainerModule, RouterModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [OmniNgModule, RouterModule, CommonModule, OmniAppModule, OmniContainerModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pfe-ui';
 }
-
-
