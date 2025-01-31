e1a4f1ae32ead8a9ba8e8346820de193e2ae0c5a
oqulustech <oqulustch@gmail.com>
2025-01-31 22:29:24 +0530

    sdasd

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  dataCompleted:Array<any> = [
+    {
+        name: "RISK_RESULTS",
+        status: "10 seconds ago",
+        action: ""
+    },
+    {
+        name: "RISK_COMPUTE_JOB",
+        status: "1 minute ago",
+        action: ""
+    },
+    {
+        name: "LIQUIDITY_DASHBOARD_DATA",
+        status: "2 hours ago",
+        action: ""
+    },
+    {
+        name: "RISK_COMPUTE_JOB",
+        status: "2 hours ago",
+        action: ""
+    },
+    {
+        name: "RISK_RESULTS",
+        status: "10 seconds ago",
+        action: ""
+    },
+    {
+        name: "RISK_COMPUTE_JOB",
+        status: "40 seconds ago",
+        action: ""
+    }
+]
 }
