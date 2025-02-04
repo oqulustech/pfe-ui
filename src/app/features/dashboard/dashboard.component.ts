29717ba4c92174e51abc656a25802a7fb9fb4ac2
oqulustech <oqulustch@gmail.com>
2025-02-04 14:55:34 -0500

    123

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -50,21 +50,6 @@ export class DashboardComponent{
     { name: "RISK_COMPUTE_JOB", status: "40 seconds ago", action: "" }
   ];
 
-  dataWaiting: Array<any> = [
-    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
-    { name: "RISK_COMPUTE_JOB", status: "1 minute ago", action: "" },
-    { name: "LIQUIDITY_DASHBOARD_DATA", status: "2 hours ago", action: "" },
-    { name: "RISK_COMPUTE_JOB", status: "2 hours ago", action: "" },
-    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
-    { name: "RISK_COMPUTE_JOB", status: "40 seconds ago", action: "" },
-    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
-    { name: "RISK_COMPUTE_JOB", status: "1 minute ago", action: "" },
-    { name: "LIQUIDITY_DASHBOARD_DATA", status: "2 hours ago", action: "" },
-    { name: "RISK_COMPUTE_JOB", status: "2 hours ago", action: "" },
-    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
-    { name: "RISK_COMPUTE_JOB", status: "40 seconds ago", action: "" }
-  ];
-
   dataRunning: Array<any> = [
     { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
     { name: "RISK_COMPUTE_JOB", status: "1 minute ago", action: "" },
@@ -80,7 +65,20 @@ export class DashboardComponent{
     { name: "RISK_COMPUTE_JOB", status: "40 seconds ago", action: "" }
   ];
 
-  
+  dataWaiting: Array<any> = [
+    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
+    { name: "RISK_COMPUTE_JOB", status: "1 minute ago", action: "" },
+    { name: "LIQUIDITY_DASHBOARD_DATA", status: "2 hours ago", action: "" },
+    { name: "RISK_COMPUTE_JOB", status: "2 hours ago", action: "" },
+    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
+    { name: "RISK_COMPUTE_JOB", status: "40 seconds ago", action: "" },
+    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
+    { name: "RISK_COMPUTE_JOB", status: "1 minute ago", action: "" },
+    { name: "LIQUIDITY_DASHBOARD_DATA", status: "2 hours ago", action: "" },
+    { name: "RISK_COMPUTE_JOB", status: "2 hours ago", action: "" },
+    { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
+    { name: "RISK_COMPUTE_JOB", status: "40 seconds ago", action: "" }
+  ];
 
   dataFailed: Array<any> = [
     { name: "RISK_RESULTS", status: "10 seconds ago", action: "" },
