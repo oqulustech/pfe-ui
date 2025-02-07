3274b72cf514e1ded2f91652d9baab0610996f93
oqulustech <oqulustch@gmail.com>
2025-02-07 10:58:37 -0500

    asd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 import { JobMonitorComponent } from './features/job-monitor/job-monitor.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { IssuesLogComponent } from './features/issues-log/issues-log.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
